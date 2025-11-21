019bbeb39f84b0809c1d3d560796a1f0aecbde03
Alonsini <alo.madrid@duocuc.cl>
2025-11-20 23:38:10 -0300

    puede ser pa?

---
+++ src/main/java/AutaZa/AutaZa/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package AutaZa.AutaZa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(
+                new Info()
+                        .title("AutaZa API")
+                        .version("1.0")
+                        .description("api para la gestion de autos en la plataforma AutaZa ouyeaa"));
+    }
+}
